8fedf38c1c07e51c23f726fb99c120d0777e4d0b
nicolascmr <nicolascmr14@gmail.com>
2025-12-21 18:55:56 +0100

    Table du produit ajouté

--- Magasin/models.py
+++ Magasin/models.py
@@ -1,3 +1,18 @@
 from .app import db
 
+class Produit(db.Model):
+    __tablename__ = 'Produit'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    reference = db.Column(db.String(50), unique=True) 
+    nom = db.Column(db.String(100))
+    quantite = db.Column(db.Integer)
+    fabricant = db.Column(db.String(100))
 
+    def __init__(self, reference, nom, quantite, fabricant):
+        self.reference = reference
+        self.nom = nom
+        self.quantite = quantite
+        self.fabricant = fabricant
+
+    def __repr__(self):
+        return f"<Produit {self.reference} - {self.nom}>"
